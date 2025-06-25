309840b16ee04f66cf4361bbdba73a0a9c7512e8
mahzcn <mahzcn@gmail.com>
2025-06-25 23:11:55 +0300

    app.js değişiklik

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,14 +1,16 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
 
-], function (Controller, MessageToast) {
+], function(Controller, MessageToast){
     "use strict";
-    return Controller.extend('ui5.product.list.controller.App', {
-        onPressCreateNewProduct: function () {
+    return Controller.extend
+    { 'ui5.product.list.controoller.App' , {
+        onPressCreateNewProduct: function() {
             MessageToast.show("Create New Product button pressed", {
-                at: 'center top'
-            });
-        }
+                at : "top center",
+            }
+            )
+    }
+    }
+    }
     });
-
-});
